bafd4afc9552b92af8901ac6e6ba66c87efecf20
mariaHello <111287457+mariaHello@users.noreply.github.com>
2024-06-07 08:27:09 -0300

    Update seu-script.js

--- seu-script.js
+++ seu-script.js
@@ -1,18 +1,20 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.promo-item');
+document.addEventListener("DOMContentLoaded", function() {
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const slides = document.querySelector('.promo-container');
+    let currentIndex = 0;
+    
+    function showSlide(index) {
+        slides.style.transform = `translateX(-${index * 100}%)`; // Move o slide atual para a esquerda
+    }
 
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(-${index * 100}%)`; // Move o slide atual para a esquerda
+    prevButton.addEventListener('click', () => {
+        currentIndex = Math.max(currentIndex - 1, 0); // Garante que o índice não seja menor que 0
+        showSlide(currentIndex);
     });
-}
 
-document.querySelector('.prev').addEventListener('click', () => {
-    currentIndex = Math.max(currentIndex - 1, 0); // Garante que o índice não seja menor que 0
-    showSlide(currentIndex);
-});
-
-document.querySelector('.next').addEventListener('click', () => {
-    currentIndex = Math.min(currentIndex + 1, slides.length - 1); // Garante que o índice não seja maior que o número total de slides
-    showSlide(currentIndex);
+    nextButton.addEventListener('click', () => {
+        currentIndex = Math.min(currentIndex + 1, slides.children.length - 1); // Garante que o índice não seja maior que o número total de slides
+        showSlide(currentIndex);
+    });
 });
